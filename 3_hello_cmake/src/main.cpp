e28c5d5caaecdfd5d79a9daf7a2f031321de6ba3
xiaosong <2359035384@qq.com>
2026-01-08 16:59:17 +0800

    学习cmake，还是需要学习一下的。

---
+++ 3_hello_cmake/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "head.h"
+
+int main() {
+    using namespace std;
+    int a = 10, b = 8;
+    cout << "a+b=" << add(a, b) << endl;
+    cout << "a-b=" << sub(a, b) << endl;
+    cout << "a*b=" << mul(a, b)<< endl;
+    cout << "a/b=" << my_div(a, b) << endl;
+    return 0;
+}
