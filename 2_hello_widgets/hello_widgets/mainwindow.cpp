182a3128c6506ceb61ad4be41ef8c659c82f8caf
xiaosong <2359035384@qq.com>
2026-01-14 16:54:05 +0800

    do it.

--- 2_hello_widgets/hello_widgets/mainwindow.cpp
+++ 2_hello_widgets/hello_widgets/mainwindow.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
-
+#include <QMenuBar>
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent) {
+        // 1、菜单栏
+        QMenuBar* qb = new QMenuBar(this);
+        // 1.1、菜单
+
+        // 1.2、菜单项
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
 }
