6498287b2434d7283f1f5a0a0442859690aec318
xiaosong <2359035384@qq.com>
2026-01-15 16:40:29 +0800

    qt官方mainwindow menu教程

--- 2_hello_widgets/hello_widgets/mainwindow.h
+++ 2_hello_widgets/hello_widgets/mainwindow.h
@@ -1,11 +1,70 @@
 #pragma once
 
 #include <QMainWindow>
-
+QT_BEGIN_NAMESPACE
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
+QT_END_NAMESPACE
+// QMainWindow继承自QWidget
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent* event) override;  // 本类对继承自QMainWindow中的方法进行重写
+#endif
+private:
+    void newFile();
+    void open();
+    void save();
+    void print();
+    void undo();
+    void redo();
+    void cut();
+    void copy();
+    void paste();
+    void bold();
+    void italic();
+    void leftAlign();
+    void rightAlign();
+    void justify();
+    void center();
+    void setLineSpacing();
+    void setParagraphSpacing();
+    void about();
+    void aboutQt();
 
+private:
+    void createActions();
+    void createMenus();
+    QMenu* fileMenu;
+    QMenu* editMenu;
+    QMenu* formatMenu;
+    QMenu* helpMenu;
+    QActionGroup* alignmentGroup;
+    QAction* newAct;
+    QAction* openAct;
+    QAction* saveAct;
+    QAction* printAct;
+    QAction* exitAct;
+    QAction* undoAct;
+    QAction* redoAct;
+    QAction* cutAct;
+    QAction* copyAct;
+    QAction* pasteAct;
+    QAction* boldAct;
+    QAction* italicAct;
+    QAction* leftAlignAct;
+    QAction* rightAlignAct;
+    QAction* justifyAct;
+    QAction* centerAct;
+    QAction* setLineSpacingAct;
+    QAction* setParagraphSpacingAct;
+    QAction* aboutAct;
+    QAction* aboutQtAct;
+    QLabel* infoLabel;
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
